e59f44a54d9007c13199f6f9982c603290198a71
hardingcheng <hardingcheng@163.com>
2021-08-12 18:19:35 +0800

    项目初始化

---
+++ layouts/default.vue
@@ -0,0 +1,24 @@
+<template>
+  <el-container class="layout-default">
+    <el-header>
+      <my-header/>
+    </el-header>
+    <el-main>
+      <Nuxt />
+    </el-main>
+    <el-footer>
+      Footer
+    </el-footer>
+  </el-container>
+</template>
+<script>
+import MyHeader from '@/components/common/header/index.vue'
+export default {
+  components:{
+    MyHeader,
+  }
+}
+</script>
+<style>
+
+</style>
